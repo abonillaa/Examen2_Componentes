191420e5343812c298d1b2ce3d05c0813dfebe84
Andres Bonilla <abonillaa@ucenfotec.ac.cr>
2020-07-13 22:23:37 -0600

    Initial commit

---
+++ src/main/java/com/cenfotec/pomponio/SegundoExamenApplication.java
@@ -0,0 +1,13 @@
+package com.cenfotec.pomponio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoExamenApplication.class, args);
+	}
+
+}
